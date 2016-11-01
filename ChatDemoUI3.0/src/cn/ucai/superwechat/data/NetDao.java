41a7b3b8a539dc96968d38da7bf4d2415e7e3bc7
teemoleary <727630072@qq.com>
2016-11-01 20:18:51 +0800

    修改注册界面布局，修改注册Activity对其进行逻辑调整，添加NetDao

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/data/NetDao.java
@@ -0,0 +1,112 @@
+package cn.ucai.superwechat.data;
+
+import android.content.Context;
+
+import java.io.File;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.bean.Result;
+import cn.ucai.superwechat.utils.MD5;
+
+
+public class NetDao {
+
+    /**
+     * 注册账号
+     * @param context
+     * @param userName
+     * @param nickName
+     * @param password
+     * @param listener
+     */
+    public static void register(Context context, String userName, String nickName, String password, OkHttpUtils.OnCompleteListener<Result> listener) {
+        OkHttpUtils<Result> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME, userName)
+                .addParam(I.User.NICK, nickName)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .targetClass(Result.class)
+                .post()
+                .execute(listener);
+    }
+
+    /**
+     * 取消注册
+     * @param context
+     * @param userName
+     * @param listener
+     */
+    public static void unregister(Context context, String userName, OkHttpUtils.OnCompleteListener<Result> listener) {
+        OkHttpUtils<Result> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UNREGISTER)
+                .addParam(I.User.USER_NAME, userName)
+                .targetClass(Result.class)
+                .execute(listener);
+    }
+
+    /**
+     * 用户登录
+     * @param context
+     * @param userName
+     * @param password
+     * @param listener
+     */
+    public static void login(Context context, String userName, String password, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME, userName)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    /**
+     * 更新用户昵称
+     * @param context
+     * @param userName
+     * @param nick
+     * @param listener
+     */
+    public static void updateNick(Context context, String userName, String nick, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
+                .addParam(I.User.USER_NAME, userName)
+                .addParam(I.User.NICK, nick)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    /**
+     * 跟新用户头像
+     * @param context
+     * @param userName
+     * @param file
+     * @param listener
+     */
+    public static void updateAvatar(Context context, String userName, File file, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID, userName)
+                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .targetClass(String.class)
+                .post()
+                .execute(listener);
+    }
+
+    /**
+     * 更新用户信息
+     * @param context
+     * @param userName
+     * @param listener
+     */
+    public static void syncUserInfo(Context context, String userName, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_USER)
+                .addParam(I.User.USER_NAME, userName)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+
+}
