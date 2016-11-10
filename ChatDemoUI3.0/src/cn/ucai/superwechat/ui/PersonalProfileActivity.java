1c013ddeb168fdc67268efb605d1d66276d6ee08
teemoleary <727630072@qq.com>
2016-11-10 11:49:35 +0800

    设置视频通话

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/PersonalProfileActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/PersonalProfileActivity.java
@@ -1,11 +1,14 @@
 package cn.ucai.superwechat.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.domain.User;
 import com.hyphenate.easeui.utils.EaseUserUtils;
 
@@ -89,6 +92,14 @@ public class PersonalProfileActivity extends BaseActivity {
                 MFGT.gotoAddFriendActivity(this,user.getMUserName());
                 break;
             case R.id.btn_chat:
+                if (!EMClient.getInstance().isConnected())
+                    Toast.makeText(this, R.string.not_connect_to_server, Toast.LENGTH_SHORT).show();
+                else {
+                    startActivity(new Intent(this, VideoCallActivity.class).putExtra("username", user.getMUserName())
+                            .putExtra("isComingCall", false));
+                    // videoCallBtn.setEnabled(false);
+//                    inputMenu.hideExtendMenuContainer();
+                }
                 break;
         }
     }
